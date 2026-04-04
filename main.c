750c9a1c9686a558564e129dfe383161e30610f4
mohamed amdouni <midouamdouni4@gmail.com>
2026-04-04 11:28:01 +0000

    added depth manually , but the result isnt satisfying , need to make it better , so i m gonna use formula instead of a 18 if/else loop

--- main.c
+++ main.c
@@ -8,11 +8,11 @@
 
 // raycasting was such a beautiful concept , yet , its not enough , yes it was difficult , and the dda conceptas well , which i m gonna add , but they re not challenging enough to satisfy..
 #include "headers.h"
-#define FOV (3.14f / 2.9f)
+#define FOV (3.14f / 2.5f)
 
 int main(void){
-    float playerx=5.5f;
-    float playery=5.5f;
+    float playerx = 5.5f;
+    float playery= 5.5f;
     int playercol;
     int playerrow;
     float playerangle;
@@ -31,24 +31,16 @@ int main(void){
                 running=false;
             }else if(event.type==SDL_KEYDOWN){
                 SDL_Keycode key=event.key.keysym.sym;
-                if(key==SDLK_UP){
+                if(key==SDLK_UP){   
                     
-                    if(playerdir==1){
-                        int nextcol = (int)(playerx + 0.1f);
-                        if(damap[playerrow][nextcol]==0){
-                        playerx+=0.1;}
-                    }else if (playerdir==2){
-                        int nextrow = (int)(playery - 0.1f);
-                        if(damap[nextrow][playercol]==0){
-                            playery-=0.1;}
-                    }else if(playerdir==3){
-                        int nextcol = (int)(playerx - 0.1f);
-                        if(damap[playerrow][nextcol]==0){
-                        playerx-=0.1;}
-                    }else if (playerdir==4){
-                        int nextrow = (int)(playery + 0.1f);
-                        if(damap[nextrow][playercol]==0){
-                            playery+=0.1;}}
+                    float nextx = playerx + cos(playerangle) * 0.1f;
+                    float nexty = playery + sin(playerangle) * 0.1f; 
+                    
+                    
+                    if(damap[(int)(nexty)][(int)(nextx)]==0){
+                        playery=nexty;
+                        playerx=nextx;}
+                    
                     
                 
                 
@@ -119,6 +111,7 @@ int main(void){
                 }else if((3*(3.14f)/2)<=playerangle && playerangle<=2*(3.14f)){
                     playerdir=4;
                 }
+                
 
 
 
@@ -128,7 +121,10 @@ int main(void){
         }
         SDL_SetRenderDrawColor(renderer,0,0,0,255);
         SDL_RenderClear(renderer);
+        
         for(int h=0;h<1280;h++){
+
+            
             float dotx=playerx;
             float doty=playery;
             float rayangle = playerangle + (h-640)*(FOV/1280); 
@@ -141,10 +137,49 @@ int main(void){
                 
             }
             float d=sqrt((pow((dotx-playerx),2)+pow(doty-playery,2)));
+            printf("%d",d);
             float his= (float)(720/d);
-            float top=360+his/2;
-            float bottom=360-his/2;
-            SDL_SetRenderDrawColor(renderer,255,255,255,255);
+            float top=360-his/2;
+            float bottom=360+his/2;
+            if(d<=0.5f){
+                SDL_SetRenderDrawColor(renderer,255,255,255,255);
+            }else if (0.5f<d && d<=1.0f){
+                SDL_SetRenderDrawColor(renderer,248,248,248,255);
+            }else if (1.0f<d && d<=1.5f){
+                SDL_SetRenderDrawColor(renderer,240,240,240,255);
+            }else if (1.5f<d && d<=2.0f){
+                SDL_SetRenderDrawColor(renderer,225,225,225,255);
+            }else if (2.0f<d && d<=2.5f){
+                SDL_SetRenderDrawColor(renderer,210,210,210,255);
+            }else if (2.5f<d && d<=3.0f){
+                SDL_SetRenderDrawColor(renderer,195,195,195,255);
+            }else if (3.0f<d && d<=3.5f){
+                SDL_SetRenderDrawColor(renderer,180,180,180,255);
+            }else if (3.5f<d && d<=4.0f){
+                SDL_SetRenderDrawColor(renderer,165,165,165,255);
+            }else if (4.0f<d && d<=4.5f){
+                SDL_SetRenderDrawColor(renderer,150,150,150,255);
+            }else if (4.5f<d && d<=5.0f){
+                SDL_SetRenderDrawColor(renderer,135,135,135,255);
+            }else if (5.0f<d && d<=5.5f){
+                SDL_SetRenderDrawColor(renderer,120,120,120,255);
+            }else if (5.5f<d && d<=6.0f){
+                SDL_SetRenderDrawColor(renderer,105,105,105,255);
+            }else if (6.0f<d && d<=6.5f){
+                SDL_SetRenderDrawColor(renderer,90,90,90,255);
+            }else if (6.5f<d && d<=7.0f){
+                SDL_SetRenderDrawColor(renderer,75,75,75,255);
+            }else if (7.0f<d && d<=7.5f){
+                SDL_SetRenderDrawColor(renderer,60,60,60,255);
+            }else if (7.5f<d && d<=8.0f){
+                SDL_SetRenderDrawColor(renderer,45,45,45,255);
+            }else if (8.0f<d && d<=8.5f){
+                SDL_SetRenderDrawColor(renderer,30,30,30,255);
+            }else if (8.5f<d && d<=9.0f){
+                SDL_SetRenderDrawColor(renderer,15,15,15,255);
+            }else if (9.0f<d){
+                SDL_SetRenderDrawColor(renderer,0,0,0,255);
+            }
             SDL_RenderDrawLine(renderer,h,top,h,bottom);
 
             
@@ -186,3 +221,5 @@ int main(void){
 
     }
 }
+
+//the dopamine of the bug fixing is addictive , i cant stop , i m seeking more the more i get
